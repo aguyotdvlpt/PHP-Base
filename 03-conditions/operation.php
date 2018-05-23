c0541a3ed4ae56a5ad5e010e008050bf95925696
aguyotdvlpt <aguyotfc@gmail.com>
2018-05-23 15:08:44 +0200

    Exercice opération

--- 03-conditions/operation.php
+++ 03-conditions/operation.php
@@ -5,7 +5,12 @@ $c = 8;
 
 $resultat1 = $a+$b+$c;
 $resultat2 = $a*($b-$c);
+if ($a > 0) { // On vérifie que le nombre a est supérieur à 0 pour la division
 $resultat3 = ($c-$b)/$a;
+} else {
+    // die('Bonjour'); // Arrête le script PHP
+    $resultat3 = 'Division par 0 impossible';
+}
 
 echo $a . " + " . $b . " + " . $c . " = " . $resultat1;
 echo "<br />";
@@ -14,10 +19,12 @@ echo "<br />";
 echo "(" . $c . " - " . $b . ") / " . $a . " = " . $resultat3;
 echo "<br />";
 
+/* INTERPOLATION 
+echo "($nombre3 - $nombre2) / $nombre1 = $resultat3";
+*/
+
 if ($resultat1 < 20 || $resultat2 < 20 || $resultat3 < 20) {
     echo "<br /> Une des opérations renvoie moins de 20";
 }
-
-
 
 ?>
