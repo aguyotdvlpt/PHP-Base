90f4d88042447157c493d13cace7b9f4fa6aaad3
aguyotdvlpt <aguyotfc@gmail.com>
2018-05-24 15:27:59 +0200

    Table de multiplication Mise en forme

--- 04-boucles/tableMultiplication.php
+++ 04-boucles/tableMultiplication.php
@@ -1,13 +1,14 @@
 
-<table>
+<table border="1" style="border-collapse: collapse">
 <?php
 
     echo "<tr>";
-for ($i =0; $i<11; $i++) {
-        if($i ==0) {
-            echo "<td>" . "x" . "</td>";
+for ($k =0; $k<11; $k++) {
+        if($k == 0) {
+            echo "<td align='center' style='width: 30px; height:30px' ><strong>" . "x" . "</strong></td>";
+            echo "<td align='center' style='width: 30px; height:30px'><strong>" . $k ."</strong></td>";
         } else {
-            echo "<td>" . $i ."</td>";
+            echo "<td align='center' style='width: 30px; height:30px'><strong>" . $k ."</strong></td>";
         }
     
 }
@@ -16,21 +17,14 @@ for ($i=0; $i<11; $i++) {
         for($j=0; $j<11; $j++) {
             
                 if ($j == 0) {
-                    echo "<td>" . $i . "</td>";
+                    echo "<td align='center' style='width: 30px; height:30px'><strong>" . $i . "</strong></td>";
+                    echo "<td align='center' style='width: 30px; height:30px'>" . $j * $i . "</td>";
                 }else{
-                    echo "<td>" . $j * $i . "</td>";
+                    echo "<td align='center' style='width: 30px; height:30px'>" . $j * $i . "</td>";
                 }
-
             }
-        
-        
     echo "</tr>";
 }   
-
-
-
-
-
 
 ?>
 </table>
