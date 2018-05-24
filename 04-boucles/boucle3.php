c17654b169470d9ddb42177a46b551a2141accb9
aguyotdvlpt <aguyotfc@gmail.com>
2018-05-24 11:52:56 +0200

    Carré et triangle avec des étoiles

--- 04-boucles/boucle3.php
+++ 04-boucles/boucle3.php
@@ -1,22 +1,35 @@
 <?php
 
+echo "<h2> 3. Ecrire le code permettant de trouver le PGCD de 2 nombres</h2>";
 $nombre1 = 845;
 $nombre2 = 312;
 $reste = null;
-$resultat = null;
+$pgcd = null;
 
-while ($nombre1 % $nombre2 !== 0) {
-    $nombre1 % $nombre2 = $reste;
-    $nombre1 = $nombre2;
-    $nombre2 = $reste;
-}
+// Le var_dump peut nous aider à comprendre le résultat d'une comparaison
+var_dump(null !== 0);
 
-
+echo 'Le PGCD de ' . $nombre1 . ' et ' .$nombre2 . ' est : ';
 
 /* 845 % 312 = 221;
 312 % 221 = 91;
 221 % 91 = 39;
 91 % 39 = 13;
 39 % 13 = 0; */
+
+
+$dividande = $nombre1;
+$divisor = $nombre2;
+while ($reste !== 0) {
+    $pgcd = $divisor; // Le PGCD potentiel
+  
+    $reste = $dividande % $divisor; // 845 % 312 = 221;
+    $dividande = $divisor; // 845 devient 312
+    $divisor = $reste; // 312 devient 221 (321 % 221 = 91);
+
+    if ($reste == 0) {
+        echo $pgcd;
+    }
+}
  
 ?>
---
+++ 04-boucles/boucle5.php
@@ -0,0 +1,33 @@
+<?php
+// Créer une boucle qui affiche 10 étoiles
+
+// Imbriquer la boucle dans une autre boucle afin d'afficher 10 lignes de 10 étoiles
+
+for ($i=0; $i<10; $i++) {
+    
+    for($j=0; $j<10; $j++){
+        echo "*";
+    }
+    echo "<br />";
+}
+
+echo '<br />';
+
+
+/* $ligne = "";
+
+for ($k=0; $k<10; $k++) {
+    
+        $ligne = $ligne . "*";
+       echo $ligne . "<br />";
+} */
+
+for ($y=0; $y<11; $y++) {
+    
+    for($x=0; $x<$y; $x++){
+        echo "*";
+    }
+    echo "<br />";
+}
+
+?>
